72fec874c321d6ccb3b66cbc87a8cf24302befa4
Aurelien <aure77+github@gmail.com>
2012-02-02 18:26:03 +0100

    Ajout du username

--- admin-pojo/src/main/java/fr/soat/devoxx/game/admin/pojo/GameResult.java
+++ admin-pojo/src/main/java/fr/soat/devoxx/game/admin/pojo/GameResult.java
@@ -32,6 +32,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name = "gameResult")
 public class GameResult {
+	private String username;
     private int nbSuccess = 0;
     private int nbFail = 0;
     private int nbInvalid = 0;
@@ -62,4 +63,12 @@ public class GameResult {
     public void setNbInvalid(int nbInvalid) {
         this.nbInvalid = nbInvalid;
     }
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
