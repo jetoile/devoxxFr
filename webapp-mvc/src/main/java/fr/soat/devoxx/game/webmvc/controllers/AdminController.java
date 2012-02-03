ddda227bd059e686bcf01d10fa0e2050676ff1af
Aurelien <aure77+github@gmail.com>
2012-02-03 17:52:35 +0100

    Ajout des appels WS rest pour la gestion des users

--- webapp-mvc/src/main/java/fr/soat/devoxx/game/webmvc/controllers/AdminController.java
+++ webapp-mvc/src/main/java/fr/soat/devoxx/game/webmvc/controllers/AdminController.java
@@ -23,17 +23,80 @@
  */
 package fr.soat.devoxx.game.webmvc.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.soat.devoxx.game.admin.pojo.dto.AllUserResponseDto;
+import fr.soat.devoxx.game.pojo.ResultResponseDto;
+import fr.soat.devoxx.game.pojo.UserResponseDto;
+import fr.soat.devoxx.game.webmvc.delegate.HttpRestException;
+import fr.soat.devoxx.game.webmvc.delegate.RequesterDelegate;
 import fr.soat.devoxx.game.webmvc.utils.TilesUtil;
 
 @Controller
 @RequestMapping(value = "/admin")
 public class AdminController {
+	
+	private static Logger logger = LoggerFactory.getLogger(AdminController.class);
+	private RequesterDelegate service;
 
 	@RequestMapping(value = { "/", "/index" })
 	public String index() {
 		return TilesUtil.DFR_ADMIN_INDEX_PAGE;
+	}
+	
+	@RequestMapping(value = "/user")
+	public String showAllUser(Model model) {
+		String forward = TilesUtil.DFR_ERRORS_ERRORMSG_PAGE;
+		try {
+			service = new RequesterDelegate("/admin/user");
+			AllUserResponseDto allUsers = service.get(AllUserResponseDto.class);
+			model.addAttribute("allUserResponses", allUsers.getUserResponses());		
+			forward = TilesUtil.DFR_ADMIN_SHOWALLUSERS_PAGE;
+		} catch (HttpRestException e) {
+			model.addAttribute("error", "admin.error.user.getall");
+			logger.info("Error while fetching all users", e);
+		}
+		return forward;
+	}
+	
+	@RequestMapping(value = "/user/{username}")
+	public String showUser(@PathVariable String username, Model model) {
+		String forward = TilesUtil.DFR_ERRORS_ERRORMSG_PAGE;
+		try {
+			service = new RequesterDelegate("/admin/user/" + username);
+			UserResponseDto userResponse = service.get(UserResponseDto.class);
+			model.addAttribute("userResponse", userResponse);
+			
+			service = new RequesterDelegate("/admin/result/" + username);
+			ResultResponseDto resultResponse = service.get(ResultResponseDto.class);
+			model.addAttribute("resultResponse", resultResponse);
+			
+			forward = TilesUtil.DFR_ADMIN_SHOWUSER_PAGE;
+		} catch (HttpRestException e) {
+			model.addAttribute("error", "admin.error.user.get");
+			model.addAttribute("errorParams", username);
+			logger.info("Error while fetching user", e);
+		}
+		return forward;
+	}
+	
+	@RequestMapping(value = "/user/{username}/delete")
+	public String removeUser(@PathVariable String username, Model model) {
+		String forward = TilesUtil.DFR_ERRORS_ERRORMSG_PAGE;
+		try {
+			service = new RequesterDelegate("/admin/user/" + username);
+			service.delete();
+			forward = "redirect:/admin/index";
+		} catch (HttpRestException e) {
+			model.addAttribute("error", "admin.error.user.delete");
+			model.addAttribute("errorParams", username);
+			logger.info("Error while deleting user", e);
+		}
+		return forward;
 	}
 }
